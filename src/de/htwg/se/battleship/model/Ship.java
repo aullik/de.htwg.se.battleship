3f08b623b201e5e9b8409e30f7a55ea2dd6b2fb5
phaldan <philipp.daniels@gmail.com>
2013-10-18 18:06:24 +0200

    Add Ship entity

---
+++ src/de/htwg/se/battleship/model/Ship.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package de.htwg.se.battleship.model;
+
+import java.util.List;
+
+/**
+ * @author phdaniel
+ *
+ */
+public class Ship {
+
+	private final List<Cell> ships;
+
+	public Ship(List<Cell> ships) {
+		this.ships = ships;
+	}
+
+	public List<Cell> getCells() {
+		return ships;
+	}
+
+}
