8654958fb235216dd98764d6481ac164de437028
phaldan <philipp.daniels@gmail.com>
2013-10-18 17:05:58 +0200

    Add Player entity

---
+++ src/de/htwg/se/battleship/model/Player.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package de.htwg.se.battleship.model;
+
+import java.util.List;
+
+/**
+ * @author phdaniel
+ *
+ */
+public class Player {
+
+	private final String name;
+	private final List<Ship> ships;
+
+	public String getName() {
+		return name;
+	}
+
+	public Player(final String name, List<Ship> ships) {
+		this.name = name;
+		this.ships = ships;
+	}
+	
+	public List<Ship> getShips() {
+		return this.ships;
+	}
+}
---
+++ test/de/htwg/se/battleship/model/PlayerTest.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package de.htwg.se.battleship.model;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author phdaniel
+ *
+ */
+public class PlayerTest {
+
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void testGetName() {
+		
+		assertEquals("philipp", new Player("philipp", null).getName());
+		assertEquals("nicolas", new Player("nicolas", null).getName());
+	}
+	
+	@Test
+	public void testGetShip() {
+	
+		List<Ship> l = new ArrayList<>();
+		Player p = new Player("philipp", l);
+		
+		assertEquals(l, p.getShips());
+		
+	}
+
+}
