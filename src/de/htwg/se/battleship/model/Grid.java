8fa1bc2b826fa5d8f2a47172f1beee36d493f82e
phaldan <philipp.daniels@gmail.com>
2013-10-18 18:33:53 +0200

    Add Grid entity

---
+++ src/de/htwg/se/battleship/model/Grid.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package de.htwg.se.battleship.model;
+
+/**
+ * @author phdaniel
+ *
+ */
+public class Grid {
+	
+	private final int size;
+
+	public Grid(final int size) {
+		this.size = size;
+	}
+
+	public Object getWidth() {
+		return size;
+	}
+
+}
---
+++ test/de/htwg/se/battleship/model/GridTest.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package de.htwg.se.battleship.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author phdaniel
+ *
+ */
+public class GridTest {
+
+	private Grid g1;
+	private Grid g2;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		g1 = new Grid(20);
+		g2 = new Grid(18);
+		
+	}
+
+	@Test
+	public void testGetSize() {
+		assertEquals(20, g1.getWidth());
+		assertEquals(18, g2.getWidth());
+	}
+
+}
