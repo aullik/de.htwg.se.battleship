aa11e9813ec75c9ef7638d4a48f8dfc0ee646220
phaldan <philipp.daniels@gmail.com>
2013-11-02 16:02:22 +0100

    Add comments to public APIs

--- src/de/htwg/se/battleship/Battleship.java
+++ src/de/htwg/se/battleship/Battleship.java
@@ -1,9 +1,21 @@
 package de.htwg.se.battleship;
 
+/**
+ * Initial class to start java program
+ * @author Philipp Daniels<philipp.daniels@gmail.com>
+ *
+ */
 public final class Battleship {
 
     private static Battleship instance = null;
 
+    private Battleship() {
+    }
+
+    /**
+     * Return always the same instance of Battleship
+     * @return Battleship instance
+     */
     public static Battleship getInstance() {
         if (instance == null) {
             instance = new Battleship();
@@ -12,9 +24,10 @@ public final class Battleship {
         return instance;
     }
 
-    private Battleship() {
-    }
-
+    /**
+     * Start java program
+     * @param args
+     */
     public static void main(String[] args) {
         Battleship.getInstance();
     }
