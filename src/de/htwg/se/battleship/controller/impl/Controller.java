087b5e33bfc67b3c2009ed0a382e44332a6b10c5
niwehrle <niwehrle@htwg>
2013-11-22 13:05:35 +0100

    changed controller

--- src/de/htwg/se/battleship/controller/impl/Controller.java
+++ src/de/htwg/se/battleship/controller/impl/Controller.java
@@ -1,6 +1,8 @@
 package de.htwg.se.battleship.controller.impl;
 
 import de.htwg.se.battleship.controller.IntController;
+import de.htwg.se.battleship.model.Grid;
+import de.htwg.se.battleship.model.Player;
 import de.htwg.se.battleship.util.observer.Observable;
 
 /**
@@ -10,9 +12,26 @@ import de.htwg.se.battleship.util.observer.Observable;
  */
 public class Controller extends Observable implements IntController {
 
+    private Player player1;
+    private Player player2;
+    private Grid   grid1;
+    private Grid   grid2;
+
     @Override
     public void updateNotify() {
         notifyObservers(null);
+    }
+
+    @Override
+    public void newgame() {
+        newgame(10);
+
+    }
+
+    @Override
+    public void newgame(final int size) {
+        grid1 = new Grid(size);
+        grid2 = new Grid(size);
     }
 
 }
