c8eef435761ed1d841c22fd8320dbc35f9099a17
phaldan <philipp.daniels@gmail.com>
2013-11-23 00:53:52 +0100

    Revert "changed controller"
    This reverts commit 087b5e33bfc67b3c2009ed0a382e44332a6b10c5.

--- src/de/htwg/se/battleship/controller/impl/Controller.java
+++ src/de/htwg/se/battleship/controller/impl/Controller.java
@@ -1,8 +1,6 @@
 package de.htwg.se.battleship.controller.impl;
 
 import de.htwg.se.battleship.controller.IntController;
-import de.htwg.se.battleship.model.Grid;
-import de.htwg.se.battleship.model.Player;
 import de.htwg.se.battleship.util.observer.Observable;
 
 /**
@@ -12,26 +10,9 @@ import de.htwg.se.battleship.util.observer.Observable;
  */
 public class Controller extends Observable implements IntController {
 
-    private Player player1;
-    private Player player2;
-    private Grid   grid1;
-    private Grid   grid2;
-
     @Override
     public void updateNotify() {
         notifyObservers(null);
-    }
-
-    @Override
-    public void newgame() {
-        newgame(10);
-
-    }
-
-    @Override
-    public void newgame(final int size) {
-        grid1 = new Grid(size);
-        grid2 = new Grid(size);
     }
 
 }
