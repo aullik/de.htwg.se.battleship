4cf77f008fdf08aa8b801d52a2336b954da3ead1
phaldan <philipp.daniels@gmail.com>
2014-01-14 18:59:17 +0100

    Extend Battleship tests to get 100% branch cover

--- test/de/htwg/se/battleship/BattleshipTest.java
+++ test/de/htwg/se/battleship/BattleshipTest.java
@@ -1,33 +1,52 @@
 package de.htwg.se.battleship;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import de.htwg.se.battleship.aview.tui.menuentry.Close;
 
 public class BattleshipTest {
 
-    @Test
-    public void test() {
+    private InputStream backup;
 
-        InputStream oldIn = System.in;
+    @Before
+    public void setUp() throws UnsupportedEncodingException {
+        backup = System.in;
 
         Close close = new Close(null);
         String s = close.command() + System.getProperty("line.separator");
+        System.setIn(new ByteArrayInputStream(s.getBytes("UTF-8")));
+    }
 
+    @After
+    public void tearDown() {
+        System.setIn(backup);
+    }
+
+    @Test
+    public void test() {
         try {
-            System.setIn(new ByteArrayInputStream(s.getBytes("UTF-8")));
             Battleship.main(null);
         } catch (Exception e) {
-            System.setIn(oldIn);
             fail(e.getMessage());
         }
 
-        System.setIn(oldIn);
+    }
+
+    @Test
+    public void testGetInstance() {
+        Battleship b1 = Battleship.getInstance();
+        Battleship b2 = Battleship.getInstance();
+
+        assertEquals(b1, b2);
     }
 
 }
