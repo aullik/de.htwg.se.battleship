df75e262385935db0fc4c122b8202814c2efa0ec
Philipp Daniels <philipp.daniels@gmail.com>
2014-05-11 03:26:20 +0200

    Disable TUI and GUI

--- src/de/htwg/se/battleship/Battleship.java
+++ src/de/htwg/se/battleship/Battleship.java
@@ -24,8 +24,8 @@ public final class Battleship {
         Injector injector = Guice.createInjector(new BattleshipModule());
 
         System.out.println("Test2");
-        injector.getInstance(MainFrame.class);
-        injector.getInstance(TextUI.class);
+        //injector.getInstance(MainFrame.class);
+        //injector.getInstance(TextUI.class);
     }
 
     /**
