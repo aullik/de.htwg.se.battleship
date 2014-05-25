90e38ccbebfcea75863d7c2ec85627d1e357fd46
Philipp Daniels <philipp.daniels@gmail.com>
2014-05-25 15:41:48 +0200

    Extend BattleshipModuleTest with comments.

--- test/de/htwg/se/battleship/BattleshipModuleTest.java
+++ test/de/htwg/se/battleship/BattleshipModuleTest.java
@@ -2,11 +2,13 @@ package de.htwg.se.battleship;
 
 import org.junit.Test;
 
+/**
+ * @author Philipp Daniels <philipp.daniels@gmail.com>
+ */
 public class BattleshipModuleTest {
 
     @Test
     public void test() {
         new BattleshipModule();
     }
-
 }
