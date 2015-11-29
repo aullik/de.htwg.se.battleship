a0a82d5fa571050070b6346ba74dbe485e2874ba
aullik <nicolaswehrle@gmx.net>
2015-11-29 12:51:45 +0100

    created new controller framework

---
+++ src/de/htwg/se/battleship/controller/gamemode/GamemodeControllable.java
@@ -0,0 +1,11 @@
+package de.htwg.se.battleship.controller.gamemode;
+
+import de.htwg.se.battleship.util.controller.Controllable;
+
+/**
+ * Interface for all Gamemodes
+ * <p>
+ * Created by aullik on 29.11.2015.
+ */
+public interface GamemodeControllable extends Controllable {
+}
